3e7bfc7244360874b752734d75b7d996e4c9374c
Patrick Blaas <patrick@kite4fun.nl>
2021-12-14 22:40:32 +0100

    update update command

--- install-update/update_insurgency_ss.sh
+++ install-update/update_insurgency_ss.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=`pwd` 
-./steamcmd +login anonymous  +force_install_dir inssa +app_update 581330 validate +quit
+./steamcmd.sh +force_install_dir inssa  +login anonymous +app_update 581330 validate +quit
